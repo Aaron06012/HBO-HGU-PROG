91c99166787599b9548b59fe92bb8f923e33d14f
Aaron06012 <aaronjeronymo06012007@gmail.com>
2026-09-08 13:25:30 +0200

    IM THE FUCKING BEST

--- test bestand.py
+++ test bestand.py
@@ -1,1 +1,14 @@
-bool("abc")
+#mark = input("What is your mark?")
+#try:
+ #   int(mark)
+#except ValueError:
+  #  try:
+   #     float(mark)
+    #except ValueError:
+    #    print("This is not a number")
+celsius = input ("Temperatuur Celsius ")
+
+Windsnelheid = input("Windsnelheid meter per seconde ")
+Luchtvochtigheid = input("Luchtvochtigheid als geheel percentage ")
+
+int(celsius, Windsnelheid, Luchtvochtigheid)
