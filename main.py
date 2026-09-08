b219b539a7420f1150330fb8afae377f8b32ef30
devriesmaxsaartje-sketch <devriesmaxsaartje@gmail.com>
2026-09-08 12:17:36 +0200

    Dit is de bestand van MAX

---
+++ main.py
@@ -0,0 +1,8 @@
+temp = float(input( "Wat is op dag 1 de temperatuur[C]: " ))
+wind = float(input("Wat is op dag 1 de windsnelheid[m/s]: "))
+vocht = float(input("Wat is op dag 1 de vochtigheid[%]: "))
+
+fahrenheit = 32 + 1.8 * temp
+
+gem = (print(F"De gemiddelde Temperatuur is: {temp} C = {fahrenheit} F"))
+gevoel = (print("De gevoelstemperatuurr is: {temp} – {vochtigheid} / 100 * {wind}."))
