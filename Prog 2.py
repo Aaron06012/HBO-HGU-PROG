426880fbe51e8b218084743921d431ee543045b3
Aaron06012 <aaronjeronymo06012007@gmail.com>
2026-09-08 11:46:52 +0200

    Initial commit

---
+++ Prog 2.py
@@ -0,0 +1,6 @@
+import random
+
+dobbelsteen_a = random.randint(1 ,6)
+dobbelsteen_b = random.randint(1 ,6)
+
+print (dobbelsteen_a + dobbelsteen_b)
---
+++ Smartapp.py
@@ -0,0 +1,14 @@
+# 1.opdracht voor smartapp het input gedeelte
+celsius = input ("Temperatuur Celsius ")
+
+if celsius > 1:
+        print("dit werk :)")
+else:
+    print("Geef mij nummer >:(")
+
+Windsnelheid = input("Windsnelheid meter per seconde ")
+Luchtvochtigheid = input("Luchtvochtigheid als geheel percentage ")
+
+print("het temperatuur vandaag is", celsius)
+print("het windsnelheid vandaag is", Windsnelheid)
+print("het luchtvochtigheid vandaag is", Luchtvochtigheid)
