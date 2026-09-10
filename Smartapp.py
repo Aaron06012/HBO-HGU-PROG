ab44e5e93fd99bebd7326abec4669f7c56a761cd
Aaron06012 <aaronjeronymo06012007@gmail.com>
2026-09-10 14:26:16 +0200

    Update Smartapp.py

--- Smartapp.py
+++ Smartapp.py
@@ -1,12 +1,12 @@
 # 1.opdracht voor smartapp het input gedeelte
 while True:
-    celsius = input ("Temperatuur Celsius ")
-    Windsnelheid = input("Windsnelheid meter per seconde ")
-    Luchtvochtigheid = input("Luchtvochtigheid als geheel percentage ")
+    celsius = float(input ("Temperatuur Celsius "))
+    Windsnelheid = float(input("Windsnelheid meter per seconde "))
+    Luchtvochtigheid = float(input("Luchtvochtigheid als geheel percentage "))
     if int(Luchtvochtigheid) <0 or int(Luchtvochtigheid) > 100 :
          print("luchtvochtigheid moet een nummer hebben tussen 0-100"),
          continue
-
+    if int(celsius) <= 0 or int(Windsnelheid) <= 0 : exit
     try:
         int(celsius)
         int(Windsnelheid)
@@ -15,6 +15,10 @@ while True:
     except:
         print("dit is geen nummer")
 
+fahrenheit = 32 + 1.8 * celsius
+gevoel = celsius - (Luchtvochtigheid / 100 * Windsnelheid)
+gem = (print(F"De gemiddelde Temperatuur is: {celsius} C = {fahrenheit} F"))
+print(gevoel)
 print("het temperatuur vandaag is", celsius)
 print("het windsnelheid vandaag is", Windsnelheid)
 print("het luchtvochtigheid vandaag is", Luchtvochtigheid)
